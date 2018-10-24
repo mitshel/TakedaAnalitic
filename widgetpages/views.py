b9edc3443fa378171ede93a1fd9918360705378c
Dmitry Shelepnev <mitshel@mail.ru>
2018-10-24 23:20:39 +0300

    Add feedback from employee filter to year list

--- widgetpages/views.py
+++ widgetpages/views.py
@@ -2,7 +2,7 @@ import json
 
 from django.shortcuts import render
 from django.db.models import Count, Sum, Min
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template.context_processors import csrf
 
 from db.models import Hs, Target, Employee
@@ -19,8 +19,8 @@ def sales_shedule(request):
     market_items = Hs.objects.values('market').distinct().order_by('market')
     # target_items = Target.objects.values('employee_name').distinct().order_by('employee_name')
     target_items = Employee.objects.order_by('name')
-    year_items = Hs.objects.values('delivery_year').distinct().order_by('delivery_year')
     entity_items = Target.objects.exclude(employee__isnull=True).values('inn','entity','id').distinct().order_by('entity')
+    year_items = Hs.objects.values('delivery_year').distinct().order_by('delivery_year')
     pivot = []
 
     for market in market_items:
@@ -49,15 +49,22 @@ def filters_employee(request):
                 empl_active = [int(e) for e in empl_active.split(',')]  if empl_active else []
                 lpu_items = Target.objects.filter(employee__in=empl_active)
                 inn_active = lpu_items.values('inn')
-                market_items = Hs.objects.filter(inn_lpu__in=inn_active).values('market').distinct().order_by('market')
-                print(market_items)
+                hs_active = Hs.objects.filter(inn_lpu__in=inn_active)
+                market_items = hs_active.values('market').distinct().order_by('market')
                 market_enabled = list(market_items)
-                data = json.dumps({'status':1, 'market_active':market_enabled})
+                year_items = hs_active.values('delivery_year').distinct().order_by('delivery_year')
+                year_enabled = list(year_items)
+                lpu_list = list(lpu_items.values('inn','entity','id').distinct().order_by('entity'))
+                print(market_enabled)
+                print(year_enabled)
+                print(lpu_list)
+                data = {'status':1, 'market_active':market_enabled, 'year_active':year_enabled,'lpu':lpu_list}
                 print(data)
 
-                response = HttpResponse()
-                response['Content-Type'] = "text/javascript"
-                response.write(json.dumps({'content': data}))
-                return response
+                # response = HttpResponse()
+                # response['Content-Type'] = "text/javascript"
+                # response.write(data)
+                # return response
+                return JsonResponse(data)
 
     return render(request,'ta_home.html', {})
