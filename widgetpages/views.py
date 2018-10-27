1104f392d6afbaa22b854caccf808ae121cbad18
Dmitry Shelepnev <mitshel@mail.ru>
2018-10-27 20:58:28 +0300

    Create Entity Table functional

--- widgetpages/views.py
+++ widgetpages/views.py
@@ -15,20 +15,28 @@ def Home(request):
 def sales_shedule(request):
     args={}
     args.update(csrf(request))
-    market_items = ['Дорипрекс', 'Тахокомб','Фендивия','Феринжект']
-    market_items = Hs.objects.values('market').distinct().order_by('market')
-    # target_items = Target.objects.values('employee_name').distinct().order_by('employee_name')
+    #market_items = ['Дорипрекс', 'Тахокомб','Фендивия','Феринжект']
+    #market_items = Hs.objects.values('market').distinct().order_by('market')
     target_items = Employee.objects.order_by('name')
     entity_items = Target.objects.exclude(employee__isnull=True).values('inn','entity','id').distinct().order_by('entity')
     year_items = Hs.objects.values('delivery_year').distinct().order_by('delivery_year')
-    pivot = []
 
+    lpu_items = Target.objects.exclude(employee__isnull=True)
+    inn_active = lpu_items.values('inn')
+    hs_active = Hs.objects.filter(inn_lpu__in=inn_active)
+    market_items = hs_active.values('market').distinct().order_by('market')
+    market_enabled = list(market_items)
+    year_items = hs_active.values('delivery_year').distinct().order_by('delivery_year')
+    year_enabled = list(year_items)
+    lpu_list = list(lpu_items.values('inn', 'entity', 'id').distinct().order_by('entity'))
+
+    pivot = []
     for market in market_items:
         data = Hs.objects.filter(market=market['market']).values('delivery_year').annotate(product_cost_sum=Sum('product_cost')).values('delivery_year','product_cost_sum')
         pivot.append({'name':market['market'],'data':data})
 
     args['pivot'] = pivot
-    args['year_min'] = Hs.objects.aggregate(delivery_year_min=Min('delivery_year'))['delivery_year_min']
+    args['year_min'] = hs_active.aggregate(delivery_year_min=Min('delivery_year'))['delivery_year_min']
     args['market'] = market_items
     args['target'] = target_items
     args['year'] = year_items
@@ -59,7 +67,7 @@ def filters_employee(request):
                 print(market_enabled)
                 print(year_enabled)
                 print(lpu_list)
-                data = {'status':1, 'market_active':market_enabled, 'year_active':year_enabled,'lpu':lpu_list}
+                data = {'entity_len':len(lpu_list), 'status':1, 'market_active':market_enabled, 'year_active':year_enabled,'lpu':lpu_list}
                 print(data)
 
                 # response = HttpResponse()
