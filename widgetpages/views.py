17d864b4419a9722f7253f77388efe78195ffc4f
Dmitry Shelepnev <mitshel@mail.ru>
2018-10-28 18:08:52 +0300

    Add full Feedback chart for target, market, year, lpu

--- widgetpages/views.py
+++ widgetpages/views.py
@@ -56,16 +56,22 @@ def filters_employee(request):
     if request.is_ajax():
         if request.method == 'POST':
             if request.POST:
-                print(">>>>",request.POST)
+                #print(">>>>",request.POST)
                 empl_active = request.POST.get('empl_active','')
                 empl_active = [int(e) for e in empl_active.split(',')]  if empl_active else []
 
                 entity_active = request.POST.get('entity_active','')
                 entity_active = [int(e) for e in entity_active.split(',')] if entity_active else []
 
+                year_active = request.POST.get('year_active','')
+                year_active = [int(e) for e in year_active.split(',')] if year_active else []
+
+                market_active = request.POST.get('market_active','')
+                market_active = [e for e in market_active.split(',')] if market_active else []
+
                 lpu_items = Target.objects.filter(employee__in=empl_active)
                 inn_active = lpu_items.filter(inn__in=entity_active).values('inn')
-                hs_active = Hs.objects.filter(inn_lpu__in=inn_active)
+                hs_active = Hs.objects.filter(inn_lpu__in=inn_active, delivery_year__in=year_active, market__in=market_active)
                 market_items = hs_active.values('market').distinct().order_by('market')
                 market_enabled = list(market_items)
                 year_items = hs_active.values('delivery_year').distinct().order_by('delivery_year')
