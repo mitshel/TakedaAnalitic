8c887a01c855186e0cfaa312b2e96ec8bf3948fe
Dmitry Shelepnev <mitshel@mail.ru>
2018-10-28 17:51:36 +0300

    Create feedback chart draw for lpu_list change

--- widgetpages/views.py
+++ widgetpages/views.py
@@ -56,18 +56,22 @@ def filters_employee(request):
     if request.is_ajax():
         if request.method == 'POST':
             if request.POST:
-                #print(">>>>",request.POST)
+                print(">>>>",request.POST)
                 empl_active = request.POST.get('empl_active','')
                 empl_active = [int(e) for e in empl_active.split(',')]  if empl_active else []
 
+                entity_active = request.POST.get('entity_active','')
+                entity_active = [int(e) for e in entity_active.split(',')] if entity_active else []
+
                 lpu_items = Target.objects.filter(employee__in=empl_active)
-                inn_active = lpu_items.values('inn')
+                inn_active = lpu_items.filter(inn__in=entity_active).values('inn')
                 hs_active = Hs.objects.filter(inn_lpu__in=inn_active)
                 market_items = hs_active.values('market').distinct().order_by('market')
                 market_enabled = list(market_items)
                 year_items = hs_active.values('delivery_year').distinct().order_by('delivery_year')
                 year_enabled = list(year_items)
                 lpu_list = list(lpu_items.values('inn','entity','id').distinct().order_by('entity'))
+                print(entity_active)
                 print(market_enabled)
                 print(year_enabled)
                 print(lpu_list)
@@ -79,11 +83,6 @@ def filters_employee(request):
                 data = {'entity_len':len(lpu_list), 'status':1, 'market_active':market_enabled, 'year_active':year_enabled,'lpu':lpu_list, 'pivot':list(pivot_data)}
                 print(data)
 
-
-                # response = HttpResponse()
-                # response['Content-Type'] = "text/javascript"
-                # response.write(data)
-                # return response
                 return JsonResponse(data)
 
     return render(request,'ta_home.html', {})
