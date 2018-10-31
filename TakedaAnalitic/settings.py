af3c516a6157fb5ab9ef3ab5379622a69b420ed5
Dmitry Shelepnev <mitshel@mail.ru>
2018-10-31 20:56:04 +0300

    Add STATIC_ROOT

--- TakedaAnalitic/settings.py
+++ TakedaAnalitic/settings.py
@@ -140,10 +140,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 
 STATICFILES_DIRS = [
     'assets',
 ]
+
+
 
 MEDIA_ROOT = BASE_DIR + '/media/'
 MEDIA_URL = '/media/'
