b0149b66f8b434246a452d6ebc0867ea46e86a66
Dmitry Shelepnev <mitshel@mail.ru>
2018-12-02 17:17:33 +0300

    Add UserProfile class and is_oradmin attribute

--- bi_auth/admin.py
+++ bi_auth/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import UserProfile
+from django.db.models import Q, F
 
 # Register your models here.
+class UserInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Доп. информация'
+
+
+# Определяем новый класс настроек для модели User
+class UserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'is_orgadmin')
+    inlines = (UserInline,)
+
+    def is_orgadmin(self, obj):
+        result = False
+        try:
+            #result = UserProfile.objects.get(user=obj).is_orgadmin
+            result = obj.user_profile.is_orgadmin
+        except:
+            pass
+        return result
+
+# Перерегистрируем модель User
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- bi_auth/models.py
+++ bi_auth/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE, related_name="user_profile")
+    is_orgadmin = models.BooleanField(verbose_name='Администратор организации')
+
+    def __unicode__(self):
+        return self.user
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
