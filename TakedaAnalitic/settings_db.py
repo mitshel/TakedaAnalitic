1862ea9107e8216826161fe8f8e96486dbae8595
Dmitry Shelepnev <mitshel@mail.ru>
2018-12-15 16:44:25 +0300

    Add Разборки с феринжектом

--- TakedaAnalitic/settings_db.py
+++ TakedaAnalitic/settings_db.py
@@ -1,9 +1,9 @@
-# DBNAME = 'CursorTest'
-# DBUSER = 'cursor'
-# DBPASSWORD = 'cursor2018'
-# DBHOST = '192.168.7.130'
-
 DBNAME = 'CursorTest'
-DBUSER = 'CursorTest'
-DBPASSWORD = 'TestCursor2018'
-DBHOST = '37.203.243.65\CURSORRPT,49175'
+DBUSER = 'cursor'
+DBPASSWORD = 'cursor2018'
+DBHOST = '192.168.7.130'
+
+# DBNAME = 'CursorTest'
+# DBUSER = 'CursorTest'
+# DBPASSWORD = 'TestCursor2018'
+# DBHOST = '37.203.243.65\CURSORRPT,49175'
