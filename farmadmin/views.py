f2922ca4fe3ac721ece2b67c415d4f7e19ef44fb
Dmitry Shelepnev <mitshel@mail.ru>
2018-12-16 21:42:37 +0300

    Add Own feature in markets

--- farmadmin/views.py
+++ farmadmin/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.views.generic import TemplateView, ListView, UpdateView, CreateView, DeleteView, RedirectView, View
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy, reverse
@@ -5,8 +7,10 @@ from django.db.models import Q
 from django_datatables_view.base_datatable_view import BaseDatatableView
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.forms.models import model_to_dict
+from django import forms
+from django.http import JsonResponse
 
-from db.models import Org, Employee, Market, Lpu, InNR, TradeNR
+from db.models import Org, Employee, Market, Lpu, InNR, TradeNR, Market_Innrs, Market_Tmnrs
 
 
 bOrgPOST = 4
@@ -190,6 +194,11 @@ class AjaxLpuAllDatatableView(BaseDatatableView):
 # Администрирование РЫНКОВ
 #
 
+class MarketAdminForm(forms.ModelForm):
+    class Meta(object):
+        model = Market
+        fields = ['id', 'org', 'name', 'innrs', 'tmnrs']
+
 class MarketsAdminView(PermissionRequiredMixin, OrgAdminMixin, BreadCrumbMixin, ListView):
     template_name = 'fa_markets.html'
     breadcrumbs = [{'name': 'Рынки', 'url': ''}]
@@ -203,6 +212,7 @@ class MarketUpdateAdminView(PermissionRequiredMixin, OrgAdminMixin, BreadCrumbMi
     """
     template_name = 'fa_market.html'
     breadcrumbs = [{'name': 'Рынки', 'url': reverse_lazy('farmadmin:markets')}]
+    # form_class = MarketAdminForm
     model = Market
     fields = ['id', 'org', 'name', 'innrs', 'tmnrs']
     permission_required = ('db.change_market', )
@@ -212,12 +222,41 @@ class MarketUpdateAdminView(PermissionRequiredMixin, OrgAdminMixin, BreadCrumbMi
         # ИСключаем все INN и ТМ уже привязанные к рынкам текущей организации
         context['innrs'] = InNR.objects.exclude(market__org=self.org).exclude(id=54656).order_by('name')   #
         context['tmnrs'] = TradeNR.objects.exclude(market__org=self.org).order_by('name')
-        #print(self.object)
-        #print(list([dict(e) for e in self.object.innrs.all().values('own=F('market_innrs__own'))]))
+        #print(list( [model_to_dict(e) for e in self.object.market_innrs_set.all()] ))
         return context
 
     def get_success_url(self):
         return reverse('farmadmin:success')
+
+    def post(self, request, *args, **kwargs):
+        if request.is_ajax():
+            if request.POST:
+                status = 'success'
+                id = int(request.POST.get('id', 0))
+                org = int(request.POST.get('org', 0))
+                name = request.POST.get('name', '')
+                innrs = json.loads(request.POST.get('innrs', ''))
+                tmnrs = json.loads(request.POST.get('tmnrs', ''))
+                try:
+                    mrkt = Market.objects.get(id=id)
+                except:
+                    mrkt = None
+                    status = 'failed'
+
+                if mrkt:
+                    mrkt.org_id = org
+                    mrkt.name = name
+                    mrkt.save()
+                    Market_Innrs.objects.filter(market=mrkt).delete()
+                    Market_Innrs.objects.bulk_create(
+                        [Market_Innrs(market_id=id, innr_id=int(e['id']), own=e['own']) for e in innrs])
+                    Market_Tmnrs.objects.filter(market=mrkt).delete()
+                    Market_Tmnrs.objects.bulk_create(
+                        [Market_Tmnrs(market_id=id, tradenr_id=int(e['id']), own=e['own']) for e in tmnrs])
+
+                return JsonResponse({'status':status})
+
+        return super().post(request, *args, **kwargs)
 
 
 class MarketCreateAdminView(PermissionRequiredMixin, OrgAdminMixin, BreadCrumbMixin, CreateView):
@@ -225,7 +264,7 @@ class MarketCreateAdminView(PermissionRequiredMixin, OrgAdminMixin, BreadCrumbMi
     breadcrumbs = [{'name': 'Рынки', 'url': reverse_lazy('farmadmin:markets')},
                    {'name': 'Новый рынок', 'url': ''}]
     model = Market
-    fields = ['org', 'name', 'innrs','tmnrs']
+    fields = ['org', 'name', 'innrs', 'tmnrs']
     permission_required = ('db.add_market',)
 
     def get_context_data(self, **kwargs):
@@ -237,6 +276,25 @@ class MarketCreateAdminView(PermissionRequiredMixin, OrgAdminMixin, BreadCrumbMi
 
     def get_success_url(self):
         return reverse('farmadmin:success')
+
+    def post(self, request, *args, **kwargs):
+        if request.is_ajax():
+            if request.POST:
+                status = 'success'
+                org = int(request.POST.get('org', 0))
+                name = request.POST.get('name', '')
+                innrs = json.loads(request.POST.get('innrs', ''))
+                tmnrs = json.loads(request.POST.get('tmnrs', ''))
+
+                id = Market.objects.create(org_id = org, name = name).id
+                Market_Innrs.objects.bulk_create(
+                    [Market_Innrs(market_id=id, innr_id=int(e['id']), own=e['own']) for e in innrs])
+                Market_Tmnrs.objects.bulk_create(
+                    [Market_Tmnrs(market_id=id, tradenr_id=int(e['id']), own=e['own']) for e in tmnrs])
+
+                return JsonResponse({'status':status})
+
+        return super().post(request, *args, **kwargs)
 
 class MarketDeleteAdminView(PermissionRequiredMixin, OrgAdminMixin, BreadCrumbMixin, DeleteView):
     template_name = 'fa_market.html'
